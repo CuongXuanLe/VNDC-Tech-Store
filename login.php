b0f589fb9f9e4c298ddcef5fa52cb89b31e33e0a
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-06-27 01:40:06 +0700

    add: login

--- login.php
+++ login.php
@@ -44,13 +44,17 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>login</title>
+   <title>Login</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
+   <!-- Bootstrap CDN -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/styleWeb.css">
+   <!-- <link rel="stylesheet" href="css/style.css"> -->
 
 </head>
 <body>
@@ -68,17 +72,22 @@ if(isset($message)){
 }
 ?>
    
-<div class="form-container">
-
-   <form action="" method="post">
-      <h3>login now</h3>
-      <input type="email" name="email" placeholder="enter your email" required class="box">
-      <input type="password" name="password" placeholder="enter your password" required class="box">
-      <input type="submit" name="submit" value="login now" class="btn">
-      <p>don't have an account? <a href="register.php">register now</a></p>
-   </form>
-
+<div id="login-form" class="container">
+   <div class="col d-flex justify-content-center align-items-center vh-100">
+      <form action="" method="post" class="card shadow-lg w-50 h-25 mt-50 p-3 border border-dark">
+         <h3 class="m-auto pb-3">LOGIN NOW</h3>
+         <input type="email" name="email" placeholder="enter your email" required class="box p-2 mb-3 border rounded border-dark">
+         <input type="password" name="password" placeholder="enter your password" required class="box p-2 border rounded border-dark">
+         <input type="submit" name="submit" value="login now" class="btn w-25 m-auto color-second-bg color-white">
+         <p class="m-auto">don't have an account? <a href="register.php" class="text-decoration-none">register now</a></p>
+      </form>
+   </div>
 </div>
+
+
+   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </body>
 </html>
