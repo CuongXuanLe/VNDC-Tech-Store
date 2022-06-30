950aa17fdaf2457f791e9aadadc64a09ef5881ac
nhut3110 <75794969+nhut3110@users.noreply.github.com>
2022-06-30 22:27:34 +0700

    fix: query error

--- shop.php
+++ shop.php
@@ -121,7 +121,7 @@ if(isset($_POST['add_to_cart'])){
    <div class="container d-flex flex-wrap justify-content-center align-items-center mb-5">
       <div class="row d-flex justify-content-center">
          <?php  
-            $select_products = mysqli_query($conn, "SELECT * FROM `products` LIMIT 6") or die('query failed');
+            $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
             if(mysqli_num_rows($select_products) > 0){
                while($fetch_products = mysqli_fetch_assoc($select_products)){
          ?>
